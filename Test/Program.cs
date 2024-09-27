c1aa8054c0a2cda1d9732dba25d5b57fe01f234c
OthmanAl-Obayyay <othmanalobayyat@gmail.com>
2024-09-27 23:58:01 +0300

    Updated program to greet user by name

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
